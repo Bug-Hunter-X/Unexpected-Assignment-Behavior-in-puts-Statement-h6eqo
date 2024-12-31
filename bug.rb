db2d75f2fa61746fbc4339332aed6a539e3e80cb
BugHunter-X <askzenai@gmail.com>
2024-12-31 16:00:05 +0100

    Add bug.rb file

---
+++ bug.rb
@@ -0,0 +1,24 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+  def value=(new_value)
+    @value = new_value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value  # Output: 10
+
+my_object.value = 20
+puts my_object.value  # Output: 20
+
+#The following line will cause an error because of unexpected behavior
+puts my_object.value = 30 # Output: 30, but unexpected assignment in puts
+```
